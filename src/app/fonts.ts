de215f5c500f1a37a5a42d84091f64f2da844ccb
ChrisCoastal <contactchriscoastal@gmail.com>
2023-11-20 09:12:27 -0800

    add demo video

--- src/app/fonts.ts
+++
@@ -1,6 +0,0 @@
-import { Red_Hat_Display } from 'next/font/google';
-
-export const redHat = Red_Hat_Display({
-  subsets: ['latin'],
-  variable: '--font-redHat',
-});
--- src/hooks/useAppContext.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import { AppContext } from '../context/AppProvider';
-
-const useAppContext = () => useContext(AppContext);
-
-export default useAppContext;
